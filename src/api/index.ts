e90b9117cf58b786235b8c91dd6365bf6fa65542
Pavel Solovev <pashius2010@gmail.com>
2022-11-21 20:02:02 +0400

    api Methods

---
+++ src/api/index.ts
@@ -0,0 +1,42 @@
+import { Octokit } from "@octokit/core";
+
+const octokit = new Octokit({
+  auth: process.env.REACT_APP_PERSONAL_ACCESS_TOKEN as string,
+});
+
+export const API = {
+  getSearchRepos: async (search?: string) => {
+    try {
+      if (search) {
+        const response = await octokit.request({
+          method: "GET",
+          url: `/search/repositories`,
+          q: encodeURIComponent(`${search}`),
+        });
+        if (response.status !== 200) throw new Error("Error fetching");
+        return response.data.items;
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("error :>> ", error.message);
+        throw error;
+      }
+    }
+  },
+  getDetail: async (name: string) => {
+    try {
+      const response = await octokit.request({
+        method: "GET",
+        url: `/repos/${name}`,
+      });
+      if (response.status !== 200) throw new Error("Error fetching");
+
+      return response.data;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("error :>> ", error.message);
+        throw error;
+      }
+    }
+  },
+};
